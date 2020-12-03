cb464a04d9c5e363630789c911efc2a4914efad1
MRayan1409 <73644456+MRayan1409@users.noreply.github.com>
2020-12-03 17:02:55 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# c24class
+# TopplingBoxes
+Toppling Boxes
---
+++ bird.js
@@ -0,0 +1,42 @@
+class Bird {
+  constructor(x, y) {
+    var options = {
+        'restitution':0.5,  //bounciness of the body
+        'friction':1.0,
+        'density':1.0
+    }
+    //Create the physics engine rectangular body
+    this.body = Bodies.rectangle(x, y, 50, 50, options);
+    
+    this.width = 50;
+    this.height = 50;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+
+    pos.x = mouseX;
+    pos.y = mouseY;
+    
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+
+    rectMode(CENTER);
+
+    //outline of the box
+    strokeWeight(4); 
+
+    //outline color of the box
+    stroke("red")
+
+    //colour of the box
+    fill("red");
+
+    //Display the box
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+};
